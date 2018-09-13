dcf085d57e940107980781ae5b01a772fdabcd8b
johannes <johannesst@gmx.de>
2018-09-13 19:45:08 +0200

    added third parsing functions file

---
+++ lib/akf_parsing_functions_three.py
@@ -0,0 +1,31 @@
+from akf_corelib.conditional_print import ConditionalPrint
+from akf_corelib.configuration_handler import ConfigurationHandler
+from .data_helper import DataHelper as dh
+from .akf_parsing_functions_common import AKFCommonParsingFunctions as cf
+from akf_corelib.regex_util import RegexUtil as regu
+
+import regex
+
+
+class AkfParsingFunctionsThree(object):
+
+    def __init__(self, endobject_factory, output_analyzer):
+        config_handler = ConfigurationHandler(first_init=False)
+
+        self.config = config_handler.get_config()
+        self.cpr = ConditionalPrint(self.config.PRINT_SEGMENT_PARSER_AKF_FN_THREE, self.config.PRINT_EXCEPTION_LEVEL,
+                                    self.config.PRINT_WARNING_LEVEL, leading_tag=self.__class__.__name__)
+
+        self.cpr.print("init akf parsing functions three")
+
+        self.ef = endobject_factory
+        self.output_analyzer = output_analyzer
+
+    def parse_something(self, real_start_tag, content_texts, content_lines, feature_lines, segmentation_class):
+        # get basic data
+        element_counter = 0
+        origpost, origpost_red, element_counter, content_texts = \
+            cf.add_check_element(self, content_texts, real_start_tag, segmentation_class, element_counter)
+
+        # logme
+        self.output_analyzer.log_segment_information(segmentation_class.segment_tag, content_texts, real_start_tag)
