86a32257d505329e1236c3dde39a281cb8205899
johannes <johannesst@gmx.de>
2019-02-12 15:14:42 +0100

    fixed LFPRIO8: Segmentation issue in Stimmrecht

--- lib/akf_segment_holder.py
+++ lib/akf_segment_holder.py
@@ -587,7 +587,7 @@ class SegmentHolder(object):
             super().__init__("Zahlstellen")
 
         def match_start_condition(self, line, line_text, line_index, features, num_lines, prev_line, combined_texts):
-            match_start, errors = regu.fuzzy_search(r"(^Zahlstellen|^Hinterlegungs\- u\.Zahlstellen)\s?:", line_text)
+            match_start, errors = regu.fuzzy_search(r"(^Zahlstellen|^Hinterlegungs\- u(nd|\.) Zahlstellen)\s?:", line_text)
 
             if match_start is not None:
                 self.do_match_work(True, match_start, line_index, errors)
